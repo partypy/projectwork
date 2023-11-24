ca963aceb3d185e4561ac9090d129a91d03e90e3
partypy <partifecske@gmail.com>
2023-11-24 10:57:08 +0100

    Create layout test and reports

--- src/test/java/hu/masterfield/layout/BudapestGOLayoutTest.java
+++ src/test/java/hu/masterfield/layout/BudapestGOLayoutTest.java
@@ -5,7 +5,6 @@ import com.galenframework.reports.GalenTestInfo;
 import com.galenframework.reports.HtmlReportBuilder;
 import com.galenframework.reports.model.LayoutReport;
 import hu.masterfield.browser.Settings;
-import hu.masterfield.pages.OpeningPage;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -14,7 +13,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -22,50 +21,37 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class BudapestGOLayoutTest {
 
-    static WebDriver driver;
-    static WebDriverWait wait;
+    public static WebDriver driver;
+    public static WebDriverWait wait;
 
-    @FindBy(xpath = "//button[text()='Elfogadom']")
-    WebElement acceptCookieButton;
-
-    @FindBy(xpath = "//input[@class='planner-from ui-widget-content ui-corner-all ui-autocomplete-input']")
-    WebElement departureInputField;
-
-    @FindBy(xpath = "//input[@class='planner-to ui-widget-content ui-corner-all ui-autocomplete-input']")
-    WebElement destinationToInputField;
-
-    @FindBy(xpath = "//input[@class='planner-button planner-plan ui-button ui-widget ui-state-default ui-corner-all']")
-    WebElement planButton;
 
     @BeforeAll
     public static void driverInit() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-
-    }
-
-    @AfterAll
-    public static void closeDriver() {
-        driver.quit();
     }
 
     @Test
-    public void budapestGOTripPlan() throws IOException {
-        driver.navigate().to(Settings.BASE_URL);
-        wait.until(ExpectedConditions.elementToBeClickable(acceptCookieButton));
-        acceptCookieButton.click();
+    public void budapestGOTripPlan() throws IOException, InterruptedException {
+        driver.get(Settings.BASE_URL);
+        WebElement cookieAcceptButtonApprears = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Elfogadom']")));
+        //WebElement acceptCookieButton = driver.findElement(By.xpath("//button[text()='Elfogadom']"));
+        cookieAcceptButtonApprears.click();
+        WebElement departureInputField = driver.findElement(By.xpath("//input[@class='planner-from ui-widget-content ui-corner-all ui-autocomplete-input']"));
         departureInputField.sendKeys("Savoya Park" + Keys.TAB);
+        WebElement destinationToInputField = driver.findElement(By.xpath("//input[@class='planner-to ui-widget-content ui-corner-all ui-autocomplete-input']"));
         destinationToInputField.sendKeys("Újpest-városkapu" + Keys.TAB);
+        WebElement planButton = driver.findElement(By.xpath("//input[@class='planner-button planner-plan ui-button ui-widget ui-state-default ui-corner-all']"));
         wait.until(ExpectedConditions.elementToBeClickable(planButton));
         planButton.click();
 
         List<String> tags = new ArrayList<>();
         LayoutReport report = Galen.checkLayout(driver, "/specs/budapestGO.gspec", tags);
+        createReport(report);
     }
 
     public void createReport(LayoutReport report) throws IOException {
@@ -75,6 +61,11 @@ public class BudapestGOLayoutTest {
 
         testInfoList.add(testInfo);
         new HtmlReportBuilder().build(testInfoList, "galen/test-report/");
+    }
+
+    @AfterAll
+    public static void closeDriver() {
+        driver.quit();
     }
 
 }
